323db6ab43d45792e5cd0b3e8ea255c6f8c7d4b1
Ole Fisker <seniorfisker62@gmail.com>
2021-02-19 11:35:53 +0100

    Update HelloWorld.js

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -3,8 +3,8 @@ import React from 'react';
 const HelloWorld = () => {
   
   function sayHello() {
-    //task = "hello":
-    alert('Hello, World!');
+    global.task = "day";
+    //alert('Hello, World!');
   }
   
   return (
