95d221c303a3891dcbb0b4b672c93f3ca93d3910
Ole Fisker <seniorfisker62@gmail.com>
2021-05-07 17:22:56 +0200

    Update Measurements.jsx

--- src/components/Measurements.jsx
+++ src/components/Measurements.jsx
@@ -1,29 +1,39 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Paper from '@material-ui/core/Paper';
-import { format } from 'date-fns';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Paper from "@material-ui/core/Paper";
+import { format } from "date-fns";
 
 const useStyles = makeStyles({
   table: {
     minWidth: 250,
   },
 });
-var x = 0;
 
-export const Measurements = ({ measurements }) => {
+export const Measurements = ({ measurements, onlyLatest }) => {
   const classes = useStyles();
   const buildTimeHeader = () => {
     if (measurements.length) {
-      return <h2> {format(measurements[0].timestamp, 'PPpp')}</h2>;
+      return <h2> {format(measurements[0].timestamp, "PP")}</h2>;
     }
   };
+
+  const buildMeasurementRow = (measurement) => (
+    <TableRow key={measurements.timestamp}>
+      <TableCell align="left">{format(measurement.timestamp, "k:m")}</TableCell>
+      <TableCell align="left">{measurement.lysthusTemp}&#8451;</TableCell>
+      <TableCell align="left">{measurement.lysthusFugt}%</TableCell>
+      <TableCell align="left">{measurement.udeTemp}&#8451;</TableCell>
+      <TableCell align="left">{measurement.udeFugt}%</TableCell>
+    </TableRow>
+  );
+
   return (
     <div>
       {buildTimeHeader()}
@@ -33,29 +43,17 @@ export const Measurements = ({ measurements }) => {
           <Table className={classes.table} aria-label="simple table">
             <TableHead>
               <TableRow>
-                <TableCell align="left">Temp. skur</TableCell>
-                <TableCell align="left">Fugt skur</TableCell>
-                <TableCell align="left">Temp. ude</TableCell>
-                <TableCell align="left">Fugt ude</TableCell>
+                <TableCell align="left">Tid</TableCell>
+                <TableCell align="left">Temp. lysthus</TableCell>
+                <TableCell align="left">Fugt lysthus</TableCell>
+                <TableCell align="left">Temp. bag lysthus</TableCell>
+                <TableCell align="left">Fugt bag lysthus</TableCell>
               </TableRow>
             </TableHead>
-            <TableBody>           
-                <TableRow >
-                  if (x) 
-                  { 
-                  <TableCell align="left">
-                    {measurements[0].lysthusTemp}&#8451;
-                  </TableCell>
-                  <TableCell align="left">
-                    {measurements[0].lysthusFugt}%
-                  </TableCell>
-                  <TableCell align="left">
-                    {measurements[0].udeTemp}&#8451;
-                  </TableCell>
-                  <TableCell align="left">
-                    {measurements[0].udeFugt}%</TableCell>
-                    }
-                </TableRow>
+            <TableBody>
+              {onlyLatest
+                ? buildMeasurementRow(measurements[0])
+                : measurements.map(buildMeasurementRow)}
             </TableBody>
           </Table>
         </TableContainer>
